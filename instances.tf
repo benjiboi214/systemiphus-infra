e0948dff1fc4fbeedb609dd9d519a8f0ac991f4e
Ben Elliot <benjiboi214@gmail.com>
2018-11-07 19:00:31 +1100

    Hoping this doesn't break the workflow (#2)

---
+++ instances.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "test_public" {
+    ami = "ami-8a3f9ae8"
+    instance_type = "t2.micro"
+    key_name = "systemiphus_ultimate_access"
+    vpc_security_group_ids = ["${aws_security_group.systemiphus_public.id}"]
+    subnet_id = "${aws_subnet.systemiphus_public_subnet.id}"
+}
+resource "aws_instance" "test_private" {
+    ami = "ami-8a3f9ae8"
+    instance_type = "t2.micro"
+    key_name = "systemiphus_ultimate_access"
+    vpc_security_group_ids = ["${aws_security_group.systemiphus_private.id}"]
+    subnet_id = "${aws_subnet.systemiphus_private_subnet.id}"
+}
--- security_groups.tf
+++ security_groups.tf
@@ -1,1 +1,71 @@
 // Define the security groups and access levels for everything here.
+resource "aws_security_group" "systemiphus_public" {
+    name = "systemiphus_public_routes"
+    description = "The SG for inbound and outbound rules to the public subnet"
+    vpc_id = "${aws_vpc.systemiphus.id}"
+
+    # # From private subnet to NAT gateway 80 & 443
+    # ingress {
+    #     from_port = 80
+    #     to_port = 80
+    #     protocol = 6
+    #     cidr_blocks = "${aws_subnet.systemiphus_private_subnet.cidr_blocks}"
+    # }
+    # ingress {
+    #     from_port = 443
+    #     to_port = 443
+    #     protocol = 6
+    #     cidr_blocks = "${aws_subnet.systemiphus_private_subnet.cidr_blocks}"
+    # }
+
+    # # From internet to webservers
+    # ingress {
+    #     from_port = 80
+    #     to_port = 80
+    #     protocol = 6
+    #     cidr_blocks = "0.0.0.0/0"
+    # }
+    # ingress {
+    #     from_port = 443
+    #     to_port = 443
+    #     protocol = 6
+    #     cidr_blocks = "0.0.0.0/0"
+    # }
+
+    # To anywhere, any protocol, any port
+
+}
+
+resource "aws_security_group" "systemiphus_private" {
+    name = "systemiphus_private_routes"
+    description = "The SG for inbound and outbound rules to the private subnet"
+    vpc_id = "${aws_vpc.systemiphus.id}"
+}
+### Public
+## Inbound
+# from current SG, All protocols, all ports
+# NAT Settings
+# from private subnet, TCP 80
+# from private subnet, TCP 443
+# webserver settings
+# from anywhere TCP 80
+# from anywhere TCP 443
+
+## Outbound
+# Basic Settings
+# to anywhere, all prots, all ports
+# Forwards SSH internally to private subnet (Optionals)
+# to private subnet, TCP 22
+# LATER: Provide access to DB Server in prtivate subnet
+
+
+## Private
+# Inbound
+# from current SG, all prot, all port
+# from public subnet, all prot, all port
+
+# Outbound
+# to the current SG, all prot, all port
+# to NAT instance, TCP 80
+# to NAT instance, TCP 443
+
