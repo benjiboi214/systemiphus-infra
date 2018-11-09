1a65207f77fa74e919a7f813f159fb918f1a70b5
Ben Elliot <benjiboi214@gmail.com>
2018-11-09 23:31:36 +1100

    Add associtations to created subnets.

--- route_tables.tf
+++ route_tables.tf
@@ -7,11 +7,21 @@ resource "aws_route_table" "systemiphus_public" {
     }
 }
 
+resource "aws_route_table_association" "systemiphus_public" {
+    route_table_id = "${aws_route_table.systemiphus_public.id}"
+    subnet_id = "${aws_subnet.systemiphus_public_subnet.id}"
+}
+
 resource "aws_route_table" "systemiphus_private" {
     vpc_id = "${aws_vpc.systemiphus.id}"
 
-    # route {
-    #     cidr_block = "0.0.0.0/0"
-    #     nat_gateway_id = "${}"
-    # }
+    route {
+        cidr_block = "0.0.0.0/0"
+        instance_id = "${aws_instance.bastion_host.id}"
+    }
+}
+
+resource "aws_route_table_association" "systemiphus_private" {
+    route_table_id = "${aws_route_table.systemiphus_private.id}"
+    subnet_id = "${aws_subnet.systemiphus_private_subnet.id}"
 }
