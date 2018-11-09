e45e510f9a9833b4bd14c49e8f8ef878f081d051
Ben Elliot <benjiboi214@gmail.com>
2018-11-09 23:33:01 +1100

    Move subnet data to null resource so it's accessible.

--- subnets.tf
+++ subnets.tf
@@ -1,12 +1,28 @@
 // Define the subnets contained inside each VPC
+
+resource "null_resource" "systemiphus_subnet" {
+    triggers = {
+        public_cidr = "${cidrsubnet(aws_vpc.systemiphus.cidr_block, 8, 0)}"
+        private_cidr = "${cidrsubnet(aws_vpc.systemiphus.cidr_block, 8, 1)}"
+    }
+}
+
 resource "aws_subnet" "systemiphus_public_subnet" {
     vpc_id = "${aws_vpc.systemiphus.id}"
-    cidr_block = "${cidrsubnet(aws_vpc.systemiphus.cidr_block, 8, 0)}"
+    cidr_block = "${null_resource.systemiphus_subnet.triggers.public_cidr}"
     availability_zone = "${var.systemiphus_aws_az}"
 }
 
 resource "aws_subnet" "systemiphus_private_subnet" {
     vpc_id = "${aws_vpc.systemiphus.id}"
-    cidr_block = "${cidrsubnet(aws_vpc.systemiphus.cidr_block, 8, 1)}"
+    cidr_block = "${null_resource.systemiphus_subnet.triggers.private_cidr}"
     availability_zone = "${var.systemiphus_aws_az}"
+}
+
+output "systemiphus_public_subnet" {
+    value = "${aws_subnet.systemiphus_public_subnet.cidr_block}"
+}
+
+output "systemiphus_private_subnet" {
+    value = "${aws_subnet.systemiphus_private_subnet.cidr_block}"
 }
