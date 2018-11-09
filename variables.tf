02749f7ea4fcd16f403c61fd6ba7f1a4c09735ca
Ben Elliot <benjiboi214@gmail.com>
2018-11-09 23:38:21 +1100

    Add vars now being tracked

--- variables.tf
+++ variables.tf
@@ -2,10 +2,16 @@
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "systemiphus_ssh_keyname" {}
 variable "systemiphus_aws_region" {}
+variable "systemiphus_bastion_host_size" {}
 variable "systemiphus_aws_az" {}
 variable "systemiphus_base_cidr_block" {}
 variable "systemiphus_region_map" {
     # Used for offseting the CIDR function based on the region for VPC and Subnet config
     type = "map"
+}
+
+variable "systemiphus_nat_instance_host_number" {
+    default = 10
 }
