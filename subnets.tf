ebbf753657ef592b0a5837ccd2605293eaa7a384
Ben Elliot <benjiboi214@gmail.com>
2018-11-10 14:26:36 +1100

    Remove the output of the internal CIDR blocks

--- subnets.tf
+++ subnets.tf
@@ -18,11 +18,3 @@ resource "aws_subnet" "systemiphus_private_subnet" {
     cidr_block = "${null_resource.systemiphus_subnet.triggers.private_cidr}"
     availability_zone = "${var.systemiphus_aws_az}"
 }
-
-output "systemiphus_public_subnet" {
-    value = "${aws_subnet.systemiphus_public_subnet.cidr_block}"
-}
-
-output "systemiphus_private_subnet" {
-    value = "${aws_subnet.systemiphus_private_subnet.cidr_block}"
-}
