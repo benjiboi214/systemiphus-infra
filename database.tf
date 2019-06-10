9661aeeb83a4f2002cf9efb2afc20aaf8a1f5422
Ben Elliot <benjiboi214@gmail.com>
2019-06-10 17:05:26 +1000

    Add postgres to stack

---
+++ database.tf
@@ -0,0 +1,26 @@
+variable "db_master_username" {}
+
+variable "db_master_password" {}
+
+
+resource "aws_db_subnet_group" "primary" {
+  name       = "sys-primary"
+  subnet_ids = ["${aws_subnet.primary_private.id}", "${aws_subnet.primary_public.id}"]
+}
+
+resource "aws_db_instance" "postgres" {
+    allocated_storage = 20
+    storage_type = "gp2"
+    engine = "postgres"
+    engine_version = "10.4"
+    instance_class = "db.t2.micro"
+    name = "systemiphus_db"
+    username = "${var.db_master_user}"
+    password = "${var.db_master_password}"
+    db_subnet_group_name = "${aws_db_subnet_group.primary.name}"
+    vpc_security_group_ids = ["${aws_security_group.private.id}"]
+}
+
+output "postgres_internal_dns" {
+    value = "${aws_db_instance.postgres.endpoint}"
+} 
