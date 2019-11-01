8d444c67cf5cd498658d8d09c7e8bd876aee91dc
Ben Elliot <benjiboi214@gmail.com>
2019-11-01 15:43:43 +1100

    Implement a production host for MMPL

--- mmpl.tf
+++ mmpl.tf
@@ -1,17 +1,23 @@
 variable "mmpl_instance_size" {}
 
-resource "null_resource" "mmpl" {
+resource "null_resource" "mmpl_staging" {
     triggers = {
-        private_ip = "${cidrhost(null_resource.subnet_ranges.triggers.public_cidr, 11)}"
+        private_ip = "${cidrhost(null_resource.subnet_ranges.triggers.public_cidr, 13)}"
     }
 }
 
-resource "aws_instance" "mmpl" {
+resource "null_resource" "mmpl_production" {
+    triggers = {
+        private_ip = "${cidrhost(null_resource.subnet_ranges.triggers.public_cidr, 12)}"
+    }
+}
+
+resource "aws_instance" "mmpl_staging" {
     ami = "${var.ubuntu_1604_ami}"
     instance_type = "${var.mmpl_instance_size}"
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.primary_public.id}"
-    private_ip = "${null_resource.mmpl.triggers.private_ip}"
+    private_ip = "${null_resource.mmpl_staging.triggers.private_ip}"
     source_dest_check = false
     vpc_security_group_ids = ["${aws_security_group.public.id}"]
 
@@ -26,11 +32,40 @@ resource "aws_instance" "mmpl" {
     }
 }
 
-resource "aws_eip" "mmpl" {
-    vpc = true
-    instance = "${aws_instance.mmpl.id}"
+resource "aws_instance" "mmpl_production" {
+    ami = "${var.ubuntu_1604_ami}"
+    instance_type = "${var.mmpl_instance_size}"
+    key_name = "${var.key_name}"
+    subnet_id = "${aws_subnet.primary_public.id}"
+    private_ip = "${null_resource.mmpl_production.triggers.private_ip}"
+    source_dest_check = false
+    vpc_security_group_ids = ["${aws_security_group.public.id}"]
+
+    root_block_device {
+        volume_type = "standard"
+        volume_size = 8
+        delete_on_termination = "true"
+    }
+
+    tags = {
+        Name = "inst-sys-mmpl-production"
+    }
 }
 
-output "mmpl_instance_public_ip" {
-    value = "${aws_eip.mmpl.public_ip}"
+resource "aws_eip" "mmpl_staging" {
+    vpc = true
+    instance = "${aws_instance.mmpl_staging.id}"
+}
+
+resource "aws_eip" "mmpl_production" {
+    vpc = true
+    instance = "${aws_instance.mmpl_production.id}"
+}
+
+output "mmpl_staging_instance_public_ip" {
+    value = "${aws_eip.mmpl_staging.public_ip}"
+}
+
+output "mmpl_production_instance_public_ip" {
+    value = "${aws_eip.mmpl_production.public_ip}"
 }
