d78311638896e25fdcc92425ecc87a39696680aa
Ben Elliot <benjiboi214@gmail.com>
2020-02-06 08:39:23 +1100

    Add version requirement to AWS provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 # Settings for the AWS provider.
 provider "aws" {
+  version = "~> 2.22"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
