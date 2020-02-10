f6958287d1d22291eb9960b709a5f4ba82f9f521
Ben Elliot <benjiboi214@gmail.com>
2020-02-10 19:19:03 +1100

    Add SSM permissions to the execution role

--- roles.tf
+++ roles.tf
@@ -25,6 +25,16 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm_read" {
+  role       = "${aws_iam_role.ecs_task_execution_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_pipeline_action" {
+  role       = "${aws_iam_role.ecs_task_execution_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineCustomActionAccess"
+}
+
 data "aws_iam_policy_document" "codedeploy_assume_role" {
   version = "2012-10-17"
   statement {
