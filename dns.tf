3beb99fb15e2054a5a36801651d3e31a853503ce
Ben Elliot <benjiboi214@gmail.com>
2020-03-22 01:41:25 +1100

    Add Mailgun Email to the platform

--- dns.tf
+++ dns.tf
@@ -12,6 +12,23 @@ variable "protonmail_dkim_txt_records" {
     type = "list"
 }
 
+variable "mailgun_spf_txt_record" {
+  type = "list"
+}
+
+variable "mailgun_krs_txt_record" {
+  type = "list"
+}
+
+variable "mailgun_mx_record" {
+  type = "list"
+}
+
+variable "mailgun_cname_record" {
+  type = "list"
+}
+
+
 # Internal DNS
 resource "aws_route53_zone" "internal_dns" {
   name = "int.systemiphus.com"
@@ -79,4 +96,48 @@ resource "aws_route53_record" "dkim_txt_records" {
   type    = "TXT"
   ttl     = "300"
   records = "${var.protonmail_dkim_txt_records}"
+}
+
+resource "aws_route53_zone" "mg_dns" {
+  name = "mg.systemiphus.com"
+}
+
+resource "aws_route53_record" "mg" {
+  zone_id = "${aws_route53_zone.public_dns.zone_id}"
+  name    = "mg"
+  type    = "NS"
+  ttl     = "300"
+  records = "${aws_route53_zone.mg_dns.name_servers}"
+}
+
+resource "aws_route53_record" "mg_txt_record_mailgun" {
+  zone_id = "${aws_route53_zone.mg_dns.zone_id}"
+  name    = "mg.systemiphus.com"
+  type    = "TXT"
+  ttl     = "300"
+  records = "${var.mailgun_spf_txt_record}"
+}
+
+resource "aws_route53_record" "mg_txt_record_mailgun_krs" {
+  zone_id = "${aws_route53_zone.mg_dns.zone_id}"
+  name    = "mx._domainkey.mg.systemiphus.com"
+  type    = "TXT"
+  ttl     = "300"
+  records = "${var.mailgun_krs_txt_record}"
+}
+
+resource "aws_route53_record" "mx_mg" {
+  zone_id = "${aws_route53_zone.mg_dns.zone_id}"
+  name    = "mg.systemiphus.com"
+  type    = "MX"
+  ttl     = "300"
+  records = "${var.mailgun_mx_record}"
+}
+
+resource "aws_route53_record" "cname_mg" {
+  zone_id = "${aws_route53_zone.mg_dns.zone_id}"
+  name    = "email.mg.systemiphus.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = "${var.mailgun_cname_record}"
 }
--- settings.tfvars
+++ settings.tfvars
@@ -20,6 +20,11 @@ protonmail_dmarc_txt_records = ["v=DMARC1; p=none; rua=mailto:ben.elliot@systemi
 protonmail_dkim_txt_records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCX/Es3jq2pV5BYQsoNErzwwI+m8yq58t4I0iuuddu57rvHEcp2RORgxXFOTCH8G32J7AAPAjs8BAkTNuBbmdUudKJiQkrIr5gbvQ3NrsIDd/jNGRcR14G94TJgrgae+WN4O9lBYEohBTK6iPfm9zipQ4bpnA5My+8UFAcuRQZ8pwIDAQAB"]
 
 
+mailgun_spf_txt_record = ["v=spf1 include:mailgun.org ~all"]
+mailgun_krs_txt_record = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCe9aanZgAb3jmg6s40ypXVMs9osn0kX1b6gg4iUAqU4JS57U47hHCUFaP6A5YuuI0KCKyoJyN5dAo4ebAjkK2BHlykauO4mFTMvWW8JetTyHDHtZn6KFx/Wu4T6ucPGJZ7e6sdI0Cjz+NEoaF2PZYVNOjC2tFw2/kcOD3n0FmzUQIDAQAB"]
+mailgun_mx_record = ["10 mxa.mailgun.org", "10 mxb.mailgun.org"]
+mailgun_cname_record = ["mailgun.org"]
+
 # ECS Settings
 ecs_task_execution_role_name = "ecsTaskExecution-systemiphus"
 jenkins_master_task_definition_arn = "arn:aws:ecs:ap-southeast-2:413514076128:task-definition/jenkins_sys_td:6"
